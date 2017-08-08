eba8a337f0eab04bd932d87f6983278c0110290d
rikkodo <ctrlalt.exit@gmail.com>
2017-08-08 23:31:04 +0900

    pygetch を削除， readchar に変更 出力を追加

--- solomix.py
+++ solomix.py
@@ -5,8 +5,11 @@ import sys
 import pyaudio
 import wave
 import numpy
+import threading
+import readchar
 
 CHUNK = 1024 * 2
+MAX_CHANNEL = 10
 
 # 設定内容
 ACTIVE = []
@@ -14,6 +17,7 @@ FILENAMES = []
 SKIP = []
 TITLE = []
 LEN = 0
+ALLFIN = False
 
 
 def readConf(conffile):
@@ -31,19 +35,26 @@ def readConf(conffile):
         if (line[0] == '#'):
             continue
 
-# バージョン
-        if (line[0] == 'v'):
+        lines = line.rstrip().split(',')
+
+        if (lines[0] == 'verison'):
+            # バージョン
             continue
-# 音声
-        else:
-            lines = line.rstrip().split(',')
+        elif (lines[0] == 'song'):
+            # 音声
             count += 1
-            TITLE.append(lines[0])
-            FILENAMES.append(lines[1])
-            SKIP.append(int(lines[2]))
+            TITLE.append(lines[1])
+            FILENAMES.append(lines[2])
+            SKIP.append(int(lines[3]))
             ACTIVE.append(1)
+            print "[%d] %s" % (count % MAX_CHANNEL, lines[1])
+        else:
+            continue
 
     LEN = count
+    if (count > MAX_CHANNEL):
+        print "Too many sound."
+        exit(1)
 
 
 def playSound():
@@ -52,17 +63,18 @@ def playSound():
     global SKIP
     global TITLE
     global LEN
+    global ALLFIN
 
     wf = []
     smpwidth = 0
     channels = 0
     framerate = 0
 
-# 楽曲ファイルオープン
+    # 楽曲ファイルオープン
     for i in range(LEN):
         wf.append(wave.open(FILENAMES[i], 'rb'))
         wf[i].readframes(SKIP[i])
-# フォーマットチェック
+        # フォーマットチェック
         if (i == 0):
             smpwidth = wf[0].getsampwidth()
             channels = wf[0].getnchannels()
@@ -74,43 +86,47 @@ def playSound():
                 print >> sys.stderr, "Format not match %d." % (i)
                 exit(1)
 
-# 出力ストリーム作成
+    # 出力ストリーム作成
     p = pyaudio.PyAudio()
     stream = p.open(format=p.get_format_from_width(smpwidth),
                     channels=channels,
                     rate=framerate,
                     output=True)
 
-# 楽曲再生
-    while (True):
+    # 楽曲再生
+    while ALLFIN is False:
         fin = False
 
-# 音声合成
+        # 音声合成
+        act = ACTIVE[:]  # 配列全コピー
+        # actSum = sum(act)
         for i in range(LEN):
             data = wf[i].readframes(CHUNK)
             if (data == ''):
                 fin = True
                 break
             data16 = numpy.frombuffer(data, dtype="int16")
-            data64 = data16.astype("int64")
+            data64 = data16.astype("int64") * act[i]
             if (i == 0):
                 dlen = len(data)
                 idata = data64
             else:
-# 一ファイル終了したら全ファイル終了．
-# TODO: どれか1ファイルアクティブだったら調整．
+                # 一ファイル終了したら全ファイル終了．
+                # TODO: どれか1ファイルアクティブだったら調整．
                 if (dlen != len(data)):
                     fin = True
                     break
                 idata += data64
         if (fin is True):
             break
+        # if (actSum != 0):
+        #     idata = idata / actSum
         idata = idata / LEN
         idata = idata.astype("int16")
         bdata = idata.tobytes()
         stream.write(bdata)
 
-# 終了処理
+    # 終了処理
     for i in range(LEN):
         wf[i].close()
 
@@ -119,6 +135,57 @@ def playSound():
 
     p.terminate()
 
+    ALLFIN = True
+
+
+def keyCheck():
+    global ACTIVE
+    global ALLFIN
+    global LEN
+    soloFlag = False
+
+    print "\n"
+
+    while ALLFIN is False:
+        for i in range(LEN):
+            if (ACTIVE[i] == 1):
+                print "[%d] " % ((i + 1) % MAX_CHANNEL),
+            else:
+                print "[ ] ",
+        if (soloFlag is True):
+            print "Solo",
+        else:
+            print "    ",
+        print "\r",
+
+        key = readchar.readchar()
+        key = ord(key)
+        if (ord('0') <= key and key < MAX_CHANNEL + ord('0')):
+            if (key == ord('0')):
+                cnt = MAX_CHANNEL
+            else:
+                cnt = key - ord('0')
+            if (cnt <= LEN):
+                if (soloFlag is True):
+                    for i in range(LEN):
+                        ACTIVE[i] = 0
+                ACTIVE[cnt - 1] = 1 - ACTIVE[cnt - 1]
+
+        elif (key == ord('a')):
+            for i in range(LEN):
+                ACTIVE[i] = 1
+
+        elif (key == ord('r')):
+            for i in range(LEN):
+                ACTIVE[i] = 1 - ACTIVE[i]
+
+        elif (key == ord('s')):
+            soloFlag = not soloFlag
+
+        elif (key == ord('q')):
+            print "\nQuit."
+            ALLFIN = True
+
 
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
@@ -126,4 +193,8 @@ if __name__ == "__main__":
         exit(1)
 
     readConf(sys.argv[1])
-    playSound()
+
+    soundThread = threading.Thread(target=playSound)
+    keyThread = threading.Thread(target=keyCheck)
+    soundThread.start()
+    keyThread.start()
